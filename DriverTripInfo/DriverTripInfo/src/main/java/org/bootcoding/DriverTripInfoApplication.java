e0a9ac4cc4d5b3a4bdc39e2bb8bd4426361fdd50
upanshu bawankar <619bawankarupanshu@gmail.com>
2024-01-19 13:57:18 +0530

    commit

---
+++ DriverTripInfo/DriverTripInfo/src/main/java/org/bootcoding/DriverTripInfoApplication.java
@@ -0,0 +1,13 @@
+package org.bootcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverTripInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriverTripInfoApplication.class, args);
+	}
+
+}
