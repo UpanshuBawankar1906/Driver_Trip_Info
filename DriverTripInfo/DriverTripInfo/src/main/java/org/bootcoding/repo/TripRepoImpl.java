f655ee39abfade54f2c68ddadbbbb1ac0abd5b8f
upanshu bawankar <619bawankarupanshu@gmail.com>
2024-01-23 12:45:08 +0530

    commit

--- DriverTripInfo/DriverTripInfo/src/main/java/org/bootcoding/repo/TripRepoImpl.java
+++ DriverTripInfo/DriverTripInfo/src/main/java/org/bootcoding/repo/TripRepoImpl.java
@@ -14,9 +14,9 @@ public class TripRepoImpl implements TripRepo{
 
     @Override
     public Trip insert(Trip trip) {
-        jdbcTemplate.update("INSERT into trip(tripid,driverid, drivername,start_location, end_location, " +
+        jdbcTemplate.update("INSERT into trip(id,driverid, drivername,start_location, end_location, " +
                         "distance, fare) " + "values(?,?,?,?,?,?,?)",
-                trip.getTripid(), trip.getDriverid(), trip.getDrivername(), trip.getStart_location(),
+                trip.getId(), trip.getDriverid(), trip.getDrivername(), trip.getStart_location(),
                 trip.getEnd_location(), trip.getDistance(), trip.getFare());
         return trip;
     }
@@ -25,16 +25,16 @@ public class TripRepoImpl implements TripRepo{
     public List<Trip> getAll() {
         return jdbcTemplate.query("select * from trip",(rs,rowNum) ->{
             return new Trip(rs.getInt(1),rs.getInt(2),rs.getString(3)
-            ,rs.getString(4),rs.getString(5),rs.getDouble(6),
+            ,rs.getString(4),rs.getString(5),rs.getFloat(6),
                     rs.getDouble(7));
         });
     }
 
     @Override
     public Trip update(Trip trip){
-        // update drivers set id=?, name=?, phoneno=?, licenseno=?, address=?, age=?, gender=?, exp=? where id = ?;
+
         jdbcTemplate.update("update trip set driverid=? , drivername=? , start_location=? , end_location=? , " +
-                        "distance=? , fare=?  where tripid=?",  trip.getTripid(), trip.getDriverid(),
+                        "distance=? , fare=?  where id=?",  trip.getId(), trip.getDriverid(),
                 trip.getDrivername(), trip.getStart_location(), trip.getEnd_location(), trip.getDistance(),
                 trip.getFare());
         return trip;
@@ -50,7 +50,7 @@ public class TripRepoImpl implements TripRepo{
     public Trip getById(int id) {
         return jdbcTemplate.queryForObject("select * from trip where id=?", (rs, rowNum) -> {
             return new Trip(rs.getInt(1), rs.getInt(2), rs.getString(3),
-                    rs.getString(4), rs.getString(5),rs.getDouble(6),
+                    rs.getString(4), rs.getString(5),rs.getFloat(6),
                     rs.getDouble(7));}, id);
     }
 }
