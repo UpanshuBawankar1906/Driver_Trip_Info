8fd2d0d805ed448af6d13599f1e9c8dc927d7354
upanshu bawankar <619bawankarupanshu@gmail.com>
2024-02-21 11:17:44 +0530

    commit

--- DriverTripInfo/DriverTripInfo/src/main/java/org/bootcoding/service/TripServiceImpl.java
+++ DriverTripInfo/DriverTripInfo/src/main/java/org/bootcoding/service/TripServiceImpl.java
@@ -1,39 +1,31 @@
 package org.bootcoding.service;
-
 import org.bootcoding.model.Drivers;
 import org.bootcoding.model.Trip;
 import org.bootcoding.repo.DriverRepo;
 import org.bootcoding.repo.TripRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 @Service
 public class TripServiceImpl implements TripService{
-
     @Autowired
     private TripRepo repo;
-
     @Override
     public void insert(Trip trip) {
         repo.insert(trip);
     }
-
     @Override
     public List<Trip> getAll() {
         return repo.getAll();
     }
-
     @Override
     public Trip update(Trip trip){
         return repo.update(trip);
     }
-
     @Override
     public void delete(int id){
         repo.delete(id);
     }
-
     @Override
     public Trip getById(int id) {
         return repo.getById(id);
