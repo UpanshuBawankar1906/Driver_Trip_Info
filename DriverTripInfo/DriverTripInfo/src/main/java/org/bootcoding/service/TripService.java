a921519cfb07f9a049dd962e38d95661819f20b4
upanshu bawankar <619bawankarupanshu@gmail.com>
2024-02-21 11:17:37 +0530

    commit

--- DriverTripInfo/DriverTripInfo/src/main/java/org/bootcoding/service/TripService.java
+++ DriverTripInfo/DriverTripInfo/src/main/java/org/bootcoding/service/TripService.java
@@ -1,19 +1,12 @@
 package org.bootcoding.service;
-
 import org.bootcoding.model.Drivers;
 import org.bootcoding.model.Trip;
-
 import java.util.List;
 
 public interface TripService {
     void insert(Trip trip);
-
     List<Trip> getAll();
-
-
     Trip update(Trip trip);
-
     void delete(int id);
-
     Trip getById(int id);
 }
